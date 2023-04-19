bfef71b558ff737762088bcbbb99534ab6adf408
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2023-04-19 13:07:42 -0700

    begin statement tests

--- tests/test_statements.py
+++ tests/test_statements.py
@@ -5,6 +5,96 @@ from intbase import ErrorType
 from interpreterv1 import Interpreter
 
 
+class TestBegin(unittest.TestCase):
+    def setUp(self) -> None:
+        self.deaf_interpreter = Interpreter(console_output=False, inp=[], trace_output=False)
+
+    def test_example1(self):
+        brewin = string_to_program('''
+            (class main
+                (field x 0)
+                (method main ()
+                    (begin
+                        (print "hello")
+                        (print "world")
+                        (print "goodbye")
+                    )
+                )
+            )
+        ''')
+
+        self.deaf_interpreter.reset()
+        self.deaf_interpreter.run(brewin)
+        output = self.deaf_interpreter.get_output()
+
+        self.assertEqual(output[0], 'hello')
+        self.assertEqual(output[1], 'world')
+        self.assertEqual(output[2], 'goodbye')
+
+    def test_example2(self):
+        brewin = string_to_program('''
+            (class main
+                (field x 0)
+                (method main ()
+                    (if (== x 0)
+                        (begin		# execute both print statements if x is zero
+                            (print "a")
+                            (print "b")
+                        )
+                    )
+                )
+            )
+        ''')
+
+        self.deaf_interpreter.reset()
+        self.deaf_interpreter.run(brewin)
+        output = self.deaf_interpreter.get_output()
+
+        self.assertEqual(output[0], 'a')
+        self.assertEqual(output[1], 'b')
+
+    def test_no_statements(self):
+        brewin = string_to_program('''
+            (class main
+                (method main ()
+                    (begin
+
+                    )
+                )
+            )
+        ''')
+        self.deaf_interpreter.run(brewin)
+
+    def test_no_statement_body(self):
+        brewin = string_to_program('''
+            (class main
+                (method main ()
+                    (begin
+                        ()
+                    )
+                )
+            )
+        ''')
+        self.assertRaises(RuntimeError, self.deaf_interpreter.run, brewin)
+
+    def test_print(self):
+        brewin = string_to_program('''
+            (class main
+                (field x 0)
+                (method main ()
+                    (print
+                        (begin
+                            (print "hello")
+                            (print "world")
+                            (print "goodbye")
+                        )
+                    )
+                )
+            )
+        ''')
+        self.assertRaises(RuntimeError, self.deaf_interpreter.run, brewin)
+
+
 class TestPrint(unittest.TestCase):
     def setUp(self) -> None:
         self.deaf_interpreter = Interpreter(console_output=False, inp=[], trace_output=False)
