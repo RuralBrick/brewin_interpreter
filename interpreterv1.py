fa123e9e01370da1f1eec1eea2d09ad2be5aa6f9
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2023-04-30 21:13:26 -0700

    Revert "unwrap expressions and statements"
    This reverts commit 96ed3cd7e883dc8bb4cbffe47f87dd3cd4b8a2cb.

--- interpreterv1.py
+++ interpreterv1.py
@@ -408,9 +408,6 @@ def evaluate_expression(expression, me: Recipe, classes: dict[SWLN, Recipe],
             if trace_output:
                 debug(f"{type(blend)=}")
             return Ingredient(blend, error, trace_output)
-        case [sub_expression]:
-            return evaluate_expression(sub_expression, me, classes, parameters,
-                                       scope, error, trace_output)
         case _:
             error(ErrorType.SYNTAX_ERROR,
                   f"Not a valid expression: {expression}")
@@ -573,8 +570,6 @@ def evaluate_statement(statement, me: Recipe, classes: dict[SWLN, Recipe],
                                                   output, error, trace_output)
                 if latest_order[0]:
                     return latest_order
-        case [sub_statement]:
-            return evaluate_statement(sub_statement)
         case _:
             error(ErrorType.SYNTAX_ERROR, f"Not a valid statement: {statement}")
     return False, None
