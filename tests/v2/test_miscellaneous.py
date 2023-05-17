768cc6e67dc2b14ce7b0af9bc8388265e25df59c
Teddy1405 <48705917+Teddy1405@users.noreply.github.com>
2023-05-17 13:32:00 -0700

    misc tests

---
+++ tests/v2/test_miscellaneous.py
@@ -0,0 +1,73 @@
+import unittest
+
+from bparser import string_to_program
+from intbase import ErrorType
+from interpreterv2 import Interpreter
+
+
+class TestClasses(unittest.TestCase):
+    def setUp(self) -> None:
+        self.deaf_interpreter = Interpreter(console_output=False, inp=[], trace_output=False)
+
+    def test_undefined(self):
+        brewin = string_to_program('''
+            (class main
+  (method void main ()
+    (call (new c) m)
+  )
+)
+        ''')
+
+        self.assertRaises(RuntimeError, self.deaf_interpreter.run, brewin)
+
+        error_type, error_line = self.deaf_interpreter.get_error_type_and_line()
+        self.assertIs(error_type, ErrorType.TYPE_ERROR)
+        self.assertEqual(error_line, 3)
+
+    def test_duplicate(self):
+        brewin = string_to_program('''
+            (class c
+  (method void m () (return))
+)
+
+(class c
+  (method void m () (return))
+)
+
+(class main
+  (method void main ()
+    (call (new c) m)
+  )
+)
+        ''')
+
+        self.assertRaises(RuntimeError, self.deaf_interpreter.run, brewin)
+
+        error_type, error_line = self.deaf_interpreter.get_error_type_and_line()
+        self.assertIs(error_type, ErrorType.TYPE_ERROR)
+        self.assertEqual(error_line, 5)
+
+
+class TestMethods(unittest.TestCase):
+    def setUp(self) -> None:
+        self.deaf_interpreter = Interpreter(console_output=False, inp=[], trace_output=False)
+
+    def test_print_void(self):
+        brewin = string_to_program('''
+            (class c
+  (method void m () (return))
+)
+
+(class main
+  (method void main ()
+    (print (call (new c) m))
+  )
+)
+        ''')
+
+        self.deaf_interpreter.reset()
+        self.deaf_interpreter.run(brewin)
+        output = self.deaf_interpreter.get_output()
+
+        self.assertEqual(output, '''None'''.splitlines())
+
